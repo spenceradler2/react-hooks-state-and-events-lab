6bc59ef1765663d2db47dc20a20ea2eac031ddea
Spencer <spencer.adler2@gmail.com>
2024-06-10 16:52:18 -0400

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart,setCart] = useState(false)
+  
+  function handleLiClick(){
+    
+    setCart((cart) => !cart)
+}
+  const currentCart = cart ? "in-cart" : ""
+  const className = cart ? "remove" : "add" 
+
   return (
-    <li className="">
+    <li className = {currentCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={className} onClick={handleLiClick}>{cart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
